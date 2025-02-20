56846ff5f9436ef5b17ab3bf6225ee1daaeca42e
AndreaTorresP <A18003912@alumnos.uady.mx>
2025-02-20 14:41:20 -0600

    Update count method and javadoc

--- LogicalLineCounter.java
+++ LogicalLineCounter.java
@@ -1,19 +1,25 @@
 import java.util.List;
 
 public class LogicalLineCounter extends LineCounter {
+    
+    /** 
+     * Counts logical lines
+     * 
+     * @param lines
+     * @return int
+     */
     @Override
     public int count(List<String> lines) {
         int numLines = 0;
 
-        for (String line : lines) {
-            if(isValidLine(line)){
-                if(isLogicalLine(line)){
-                    if(isForStructure(line)){
-                        numLines+=3;
-                    }
-                    else{
-                        numLines++;
-                    }
+        List<String> cleanLines = removeComments(lines);
+        for (String line : cleanLines) {
+            if(isLogicalLine(line)){
+                if(isForStatement(line)){
+                    numLines+=3;
+                }
+                else{
+                    numLines++;
                 }
             }
         }
@@ -21,6 +27,13 @@ public class LogicalLineCounter extends LineCounter {
         return numLines;
     }
     
+    
+    /**
+     * Verifies if it's a logical line
+     * 
+     * @param line
+     * @return boolean
+     */
     private boolean isLogicalLine(String line) {
         line = line.trim();
         
@@ -36,42 +49,111 @@ public class LogicalLineCounter extends LineCounter {
         return false;
     }
     
+    
+    /**
+     * Verifies if it is an empty or only bracket line
+     * 
+     * @param line
+     * @return boolean
+     */
     private boolean isEmptyOrBracket(String line) {
         return line.isEmpty() || line.equals("{") || line.equals("}");
     }
     
+    
+    /** 
+     * Verifies if it's a class or interface statement
+     * 
+     * @param line
+     * @return boolean
+     */
     private boolean isClassOrInterfaceStatement(String line) {
         return line.matches("(class|interface)");
     }
 
+    
+    /** 
+     * Verifies if it's a method initialization statement
+     * 
+     * @param line
+     * @return boolean
+     */
     private boolean isMethodStatement(String line) {
         return line.matches("(public|private)+[a-zA-Z+ _<>]+\\(+.*\\)+ *\\{");
     }
 
+    
+    /** 
+     * Verifies if it's a general statement
+     * 
+     * @param line
+     * @return boolean
+     */
     private boolean isStatement(String line) {
         return line.contains(";");
     }
     
+    
+    /** 
+     * Verifies if it's a try statement
+     * 
+     * @param line
+     * @return boolean
+     */
     private boolean isTryStatement(String line) {
         return line.contains("try");
     }
     
+    
+    /** 
+     * Verifies if it's part of a control structure
+     * 
+     * @param line
+     * @return boolean
+     */
     private boolean isControlStructure(String line) {
         return line.matches("\\b(if|else|switch|for|while|do|catch|finally)\\b.*");
     }
     
+    
+    /** 
+     * Verifies if it's part of a switch structure
+     * 
+     * @param line
+     * @return boolean
+     */
     private boolean isSwitchCase(String line) {
         return line.matches("\\b(case|default)\\b.*:");
     }
     
+    
+    /** 
+     * Verifies if it's a brake statement
+     * 
+     * @param line
+     * @return boolean
+     */
     private boolean isBreakStatement(String line) {
         return line.contains("break");
     }
 
-    private boolean isForStructure(String line) {
+    
+    /** 
+     * Verifies if it's a for initialization statement
+     * 
+     * @param line
+     * @return boolean
+     */
+    private boolean isForStatement(String line) {
         return line.contains("for");
     }
 
+    
+    /** 
+     * Verifies if it's an import statement
+     * @param line
+     * @return boolean
+     */
     private boolean isImportStatement(String line) {
         return line.contains("import");
     }
