43ae144a5e13bbb0126a48dcc33ca353181b247f
AndreaTorresP <A18003912@alumnos.uady.mx>
2025-02-23 12:42:09 -0600

    Esqueleto pruebas unitarias

---
+++ demo/src/test/java/com/example/LogicalLineCounterTest.java
@@ -0,0 +1,74 @@
+package com.example;
+
+import org.junit.Test;
+
+public class LogicalLineCounterTest {
+    
+    @Test
+    public void count() {
+    }
+
+    @Test
+    public void isLogicalLine() {
+    }
+
+    @Test
+    public void isEmpty() {
+    }
+
+    @Test
+    public void isBracket() {
+    }
+
+    @Test
+    public void isClassOrInterfaceStatement() {
+    }
+
+    @Test
+    public void isMethodStatement() {
+    }
+
+    @Test
+    public void isStatement() {
+    }
+
+    @Test
+    public void statementIncomplete() {
+    }
+
+    @Test
+    public void isTryStatement() {
+    }
+
+    @Test
+    public void isControlStructureWithCondition() {
+    }
+
+    @Test
+    public void isControlStructureWithoutCondition() {
+    }
+
+    @Test
+    public void isWhileStatement() {
+    }
+
+    @Test
+    public void isSwitchCase() {
+    }
+
+    @Test
+    public void isBreakStatement() {
+    }
+
+    @Test
+    public void isForStatement() {
+    }
+
+    @Test
+    public void isImportStatement() {
+    }
+
+    @Test
+    public void isPackageStatement() {
+    }
+}
