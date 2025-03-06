1fcee066de4b1635b44c123fa1149e7a31507b5a
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2025-03-05 19:48:26 -0600

    cambio en el ejemplo Example3

--- files/files2/Example3.java
+++ files/files2/Example3.java
@@ -1,6 +1,10 @@
 package files.files2;
 
 public class Example3 {
+    /* 
+      Multi-line comment
+      This should not be counted as a logical line
+     */
     public static void main(String[] args) {
         System.out.println("Hello, end world!");
     }
